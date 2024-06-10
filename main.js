e59cc06fa128a57796948a258bdc7488b86004df
Dario Jularic <dariojularic0@gmail.com>
2024-06-10 11:43:51 +0200

    Create classes

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 import './style.css'
+
+class Account{
+  constructor(fullName, movements, interestRate, pin) {
+    this.fullName = fullName;
+    this.movements = movements;
+    this.interestRate = interestRate;
+    this.id = self.crypto.randomUUID();
+    this.pin = pin;
+  }
+
+  // addTransaction() {
+
+  // }
+}
+
+class AccountManager{
+  // const accountsArray;
+  constructor() {
+    this.accountsArray = [];
+  }
+
+  addAccount(account) {
+    this.accountsArray.push(account)
+  }
+
+
+}
